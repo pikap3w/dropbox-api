01bc9d278fb2da2dd433c9a20ecd1edc5a8c15e8
Kristen <29871623+pikap3w@users.noreply.github.com>
2019-04-21 02:20:33 -0500

    package.json

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-import { Dropbox } from 'dropbox'
+import { Dropbox } from 'dropbox';
 
 const dbx = new Dropbox({
   accessToken: process.env.ACCESSTOKEN,
   fetch // same as fetch: fetch
 })
+
+// http://dropbox.github.io/dropbox-sdk-js/Dropbox.html
+
+dbx.filesListFolder({
+  path: '' // root folder of dropbox account
+}).then(res => console.log(res))

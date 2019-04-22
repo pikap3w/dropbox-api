70725f1ee218c5c3152692ef80b22fb8e7dbb165
Kristen <29871623+pikap3w@users.noreply.github.com>
2019-04-22 01:02:35 -0500

    added isomorphic-fetch package

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import { Dropbox } from 'dropbox';
+const fetch = require('isomorphic-fetch')
+const Dropbox = require('dropbox').Dropbox
 
 const dbx = new Dropbox({
   accessToken: process.env.ACCESSTOKEN,
-  fetch // same as fetch: fetch
+  fetch // same as saying fetch: fetch
 })
 
 // http://dropbox.github.io/dropbox-sdk-js/Dropbox.html
 
-dbx.filesListFolder({
-  path: '' // root folder of dropbox account
-}).then(res => console.log(res))
+dbx
+  .filesListFolder({ path: '' }) // root folder of dropbox account
+  .then(console.log, console.error)
+// }).then(res => console.log(res))
